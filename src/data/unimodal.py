c78d72a6947c5c0a8f20214050bd1886d0f70bef
Tim Cares <peep-kestrel-0t@icloud.com>
2024-02-25 22:39:22 +0100

    Adjustments

--- src/data/unimodal.py
+++ src/data/unimodal.py
@@ -14,19 +14,69 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-def get_librispeech_dataset(spectrogram:bool=True, dataset:str="train-clean-100", mask_percentage:float=0.05, consequitive:bool=False,
+class SpectrogramConsequitiveMasking():
+    def __init__(self, scale:bool=False, mask_percentage:float=0.05):
+        self.scale = scale
+        self.mask_percentage = mask_percentage
+        self.mel_spectrogram = T.MelSpectrogram()
+        self.masking = T.TimeMasking(time_mask_param=100_000, iid_masks=True, p=mask_percentage)
+
+    def __call__(self, batch):
+        specs = []
+        specs_masked = []
+        max_time_steps = 0
+        for x in batch:
+            if self.scale:
+                spec = (self.mel_spectrogram(x[0]) + 1e-6).log2()
+            else:
+                spec = self.mel_spectrogram(x[0])
+            specs.append(spec)
+            specs_masked.append(self.masking(spec))
+            current_time_steps = spec.shape[-1]
+            if current_time_steps > max_time_steps:
+                max_time_steps = current_time_steps
+
+        specs = torch.cat([torch.nn.functional.pad(w, (0, max_time_steps - w.shape[-1]), value=-1) for w in specs])
+        specs_masked = torch.cat([torch.nn.functional.pad(w, (0, max_time_steps - w.shape[-1]), value=-1) for w in specs_masked])
+
+        return specs_masked, specs
+    
+class ConsequitiveMasking():
+    def __init__(self, mask_percentage:float=0.05):
+        self.mask_percentage = mask_percentage
+
+    def __call__(self, batch):
+        waveforms = []
+        waveforms_masked = []
+        max_time_steps = 0
+        
+        for x in batch:
+            waveform = x[0]
+            waveforms.append(waveform)
+            waveform_masked = waveform.clone()
+
+            current_time_steps = waveform.shape[-1]
+            mask_length = int(current_time_steps * self.mask_percentage)
+            start = torch.randint(0, current_time_steps - mask_length, (1, )).item()
+            for idx, s in enumerate(start):
+                waveform_masked[idx, ..., s:s+mask_length] = 0.0
+            waveforms_masked.append(waveform_masked)
+            
+            if current_time_steps > max_time_steps:
+                max_time_steps = current_time_steps
+
+        waveforms = torch.cat([torch.nn.functional.pad(w, (0, max_time_steps - w.shape[-1]), value=-1) for w in waveforms])
+        waveforms_masked = torch.cat([torch.nn.functional.pad(w, (0, max_time_steps - w.shape[-1]), value=-1) for w in waveforms_masked])
+
+        return waveforms_masked, waveforms
+
+def get_librispeech_dataset(spectrogram:bool=True, scale:bool=False, dataset:str="train-clean-100", mask_percentage:float=0.05, consequitive:bool=False,
                  batch_size:int=32, shuffle:bool=True, num_workers:int=1):
     librispeech = LIBRISPEECH(root="./data", url=dataset, download=True)
 
     if spectrogram and consequitive:
-        mel_spectrogram = T.MelSpectrogram()
-        # 100_000 so that every possible wavelength can be masked up to the maximum mask percentage (mask_percentage)
-        masking = T.TimeMasking(time_mask_param=100_000, iid_masks=True, p=mask_percentage)
-
-        def collate_fn(batch):
-            return masking(mel_spectrogram(batch)), batch
-        
-        return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
+        return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                          collate_fn=SpectrogramConsequitiveMasking(scale, mask_percentage))
             
     elif spectrogram and not consequitive:
         mel_spectrogram = T.MelSpectrogram()
@@ -41,27 +91,20 @@ def get_librispeech_dataset(spectrogram:bool=True, dataset:str="train-clean-100"
             n_patches = n_patches_per_batch[i%max_batches].item()
             i+=1
             mask_length = int(num_time_steps * mask_percentage)
-            max_length_per_patch = mask_length//n_patches
-            start = torch.randint(0, num_time_steps - max_length_per_patch, (n_patches,))
-            for idx, s in enumerate(start):
-                X[idx, ..., s:s+max_length_per_patch] = 0
+            length_per_patch = mask_length//n_patches
+
+            for idx in enumerate(batch.shape[0]):
+                start = torch.randint(0, num_time_steps - length_per_patch, (n_patches,))
+                for s in start:
+                    s=s.item()
+                    X[idx, ..., s:s+length_per_patch] = 0
             return X, batch
         
         return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
 
     elif not spectrogram and consequitive:
-        max_batches = math.ceil(len(librispeech)/batch_size)
-
-        def collate_fn(batch):
-            X = batch.clone()
-            num_time_steps = X.shape[-1]
-            mask_length = int(num_time_steps * mask_percentage)
-            starts = torch.randint(0, num_time_steps - mask_length, (batch_size, ))
-            for idx, s in enumerate(starts):
-                X[idx, ..., s:s+mask_length] = 0
-            return X, batch
-        
-        return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
+        return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers,
+                          collate_fn=ConsequitiveMasking(mask_percentage))
 
     else:
         max_batches = math.ceil(len(librispeech)/batch_size)
@@ -74,10 +117,13 @@ def get_librispeech_dataset(spectrogram:bool=True, dataset:str="train-clean-100"
             n_patches = n_patches_per_batch[i%max_batches].item()
             i+=1
             mask_length = int(num_time_steps * mask_percentage)
-            max_length_per_patch = mask_length//n_patches
-            start = torch.randint(0, num_time_steps - max_length_per_patch, (n_patches,))
-            for idx, s in enumerate(start):
-                X[idx, ..., s:s+max_length_per_patch] = 0
+            length_per_patch = mask_length//n_patches
+            
+            for idx in enumerate(batch.shape[0]):
+                start = torch.randint(0, num_time_steps - length_per_patch, (n_patches,))
+                for s in start:
+                    s=s.item()
+                    X[idx, ..., s:s+length_per_patch] = 0
             return X, batch
         
         return DataLoader(librispeech, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=collate_fn)
