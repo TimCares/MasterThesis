a0a51e3f1b9c33b62fa1f6ad74588cfcdef6e482
Tim Cares <peep-kestrel-0t@icloud.com>
2024-02-27 18:30:08 +0100

    Adopted code from fairseq

--- src/models/wave2vec.py
+++ src/models/wave2vec.py
@@ -1,31 +1,94 @@
+from src.modules.norm import Fp32LayerNorm, Fp32GroupNorm
+from src.modules.transpose import TransposeLast
+from typing import *
 import torch
 import torch.nn as nn
 
-class FeatureEncoder(nn.Module):
-    def __init__(self):
-        super(FeatureEncoder, self).__init__()
-        self.channels = 512
-        self.strides = [5,2,2,2,2,2,2]
-        self.kernel_widths = [10,3,3,3,3,2,2]
-        list_conv = []
-        list_ln = []
-        for i, (kw, s) in enumerate(zip(self.kernel_widths, self.strides)):
-            if i == 0:
-                list_conv.append(nn.Conv1d(1, self.channels, kernel_size=kw, stride=s, padding=kw//2)) # TODO check padding
-            else:
-                list_conv.append(nn.Conv1d(self.channels, self.channels, kernel_size=kw, stride=s, padding=kw//2))
-            
-            list_ln.append(nn.LayerNorm(self.channels))
+class ConvFeatureExtractionModel(nn.Module):
+    def __init__(
+        self,
+        conv_layers: List[Tuple[int, int, int]],
+        dropout: float = 0.0,
+        mode: str = "default",
+        conv_bias: bool = False,
+    ):
+        super(ConvFeatureExtractionModel).__init__()
 
-        self.convs = nn.ModuleList(list_conv)
-        self.layer_norms = nn.ModuleList(list_ln)
-        self.gelu = nn.GELU()
+        def block(n_in, n_out, k, stride, is_layer_norm=False, is_group_norm=False, conv_bias=False):
+
+            def make_conv():
+                conv = nn.Conv1d(n_in, n_out, k, stride=stride, bias=conv_bias)
+                nn.init.kaiming_normal_(conv.weight)
+                return conv
+
+            if is_layer_norm:
+                return nn.Sequential(
+                    make_conv(),
+                    nn.Dropout(p=dropout),
+                    nn.Sequential(
+                        TransposeLast(),
+                        Fp32LayerNorm(n_out, elementwise_affine=True), # "n_out" was "dim" in the original code
+                        TransposeLast(),
+                    ),
+                    nn.GELU(),
+                )
+            elif is_group_norm:
+                return nn.Sequential(
+                    make_conv(),
+                    nn.Dropout(p=dropout),
+                    Fp32GroupNorm(n_out, n_out, affine=True),
+                    nn.GELU(),
+                )
+            else:
+                return nn.Sequential(make_conv(), nn.Dropout(p=dropout), nn.GELU())
+
+        in_d = 1
+        self.conv_layers = nn.ModuleList()
+        for i, cl in enumerate(conv_layers):
+            (dim, k, stride) = cl
+
+            self.conv_layers.append(
+                block(
+                    in_d,
+                    dim,
+                    k,
+                    stride,
+                    is_layer_norm=mode == "layer_norm",
+                    is_group_norm=mode == "default" and i == 0,
+                    conv_bias=conv_bias,
+                )
+            )
+            in_d = dim
 
     def forward(self, x):
-        for i in range(len(self.convs)):
-            x = self.convs[i](x)
-            x = x.transpose(1, 2) # so that normalization is done over the channels
-            x = self.layer_norms[i](x)
-            x = x.transpose(1, 2) # back to the original shape
-            x = self.gelu(x)
+
+        # BxT -> BxCxT
+        # x = x.unsqueeze(1)
+
+        for conv in self.conv_layers:
+            x = conv(x)
+
         return x
+
+channels = [512, 512, 512, 512, 512, 512, 512]
+strides = [5,2,2,2,2,2,2]
+kernel_widths = [10,3,3,3,3,2,2]
+
+feature_extractor_args = zip(channels, kernel_widths, strides)
+class MMData2Vec(nn.Module):
+    def __init__(self, embed_dim:int, audio_feature_encoder_args: List[Tuple[int, int, int]]):
+        super(MMData2Vec, self).__init__()
+        
+        self.audio_feature_extractor = ConvFeatureExtractionModel(audio_feature_encoder_args, mode='layer_norm')
+
+        feature_embed_dim = audio_feature_encoder_args[-1][0] # channels of last conv layer
+
+        self.feature_projector = nn.Sequential(
+            TransposeLast(),
+            nn.LayerNorm(feature_embed_dim),
+            nn.Linear(feature_embed_dim, embed_dim),
+        ) if embed_dim != feature_embed_dim else None
+
+
+    def forward(self, x):
+        pass
---
+++ src/modules/norm.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Fp32LayerNorm(nn.LayerNorm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input):
+        output = F.layer_norm(
+            input.float(),
+            self.normalized_shape,
+            self.weight.float() if self.weight is not None else None,
+            self.bias.float() if self.bias is not None else None,
+            self.eps,
+        )
+        return output.type_as(input)
+    
+
+class Fp32GroupNorm(nn.GroupNorm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input):
+        output = F.group_norm(
+            input.float(),
+            self.num_groups,
+            self.weight.float() if self.weight is not None else None,
+            self.bias.float() if self.bias is not None else None,
+            self.eps,
+        )
+        return output.type_as(input)
