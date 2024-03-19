76d40c2bfb3cfa8b95b168c4adb523a40c12cf98
Tim Cares <peep-kestrel-0t@icloud.com>
2024-03-19 16:55:52 +0100

    Added audio only data setup (librispeech)

---
+++ src/datasets/librispeech/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+data_dir="../../../data/"
+
+mkdir -p ${data_dir}
+
+curl -O https://openslr.elda.org/resources/12/train-clean-100.tar.gz
+# curl -O https://openslr.elda.org/resources/12/train-clean-360.tar.gz
+# curl -O https://openslr.elda.org/resources/12/train-other-500.tar.gz
+
+tar -xvf train-clean-100.tar.gz
+
+rm train-clean-100.tar.gz
+
+# tar -xvf train-clean-360.tar.gz
+
+# rm train-clean-360.tar.gz
+
+# tar -xvf train-other-500.tar.gz
+
+# rm train-other-500.tar.gz
+
+mv LibriSpeech ${data_dir}
+
+python wav2vec_manifest.py ${data_dir}LibriSpeech --dest ${data_dir}LibriSpeech --seed 42 --valid-percent 0
