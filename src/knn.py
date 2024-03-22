86cf5260de94bf3eeb671ecd41f285b99eb63d4a
Tim Cares <peep-kestrel-0t@icloud.com>
2024-03-22 22:53:40 +0100

    Added naming of knn model

--- src/knn.py
+++ src/knn.py
@@ -5,8 +5,12 @@ import os
 import pickle
 import numpy as np
 
-def get_knn_model():
-    path = os.path.join(DATA_PATH, "knn_model.pkl")
+def get_knn_model(postfix:str=None):
+    if postfix is None:
+        postfix = ""
+    else:
+        postfix = "_" + postfix
+    path = os.path.join(DATA_PATH, f"knn_model{postfix}.pkl")
     if not os.path.exists(path):
         make_knn_model
     with open(path, "rb") as f:
@@ -17,8 +21,12 @@ def make_knn_predictions(data:np.ndarray):
     knn = get_knn_model()
     return knn.predict(data)
 
-def make_knn_model(n_neighbors, X_train:np.ndarray, y_train:np.ndarray) -> None:
+def make_knn_model(n_neighbors:int, X_train:np.ndarray, y_train:np.ndarray, postfix:str=None) -> None:
     model = cuKNeighbors(n_neighbors=n_neighbors)
     model.fit(X_train, y_train)
-    with open(os.path.join(DATA_PATH, "knn_model.pkl"), "wb") as f:
+    if postfix is None:
+        postfix = ""
+    else:
+        postfix = "_" + postfix
+    with open(os.path.join(DATA_PATH, f"knn_model{postfix}.pkl"), "wb") as f:
         pickle.dump(model, f)
