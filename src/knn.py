0dad6b9c3ed1707f6e5287ce54bdb7fc0a000155
Tim Cares <peep-kestrel-0t@icloud.com>
2024-03-23 14:32:51 +0100

    Adjusted knn

--- src/knn.py
+++ src/knn.py
@@ -1,32 +1,15 @@
-import cudf, cuml
-from cuml.neighbors import KNeighborsClassifier as cuKNeighbors
-from config import DATA_PATH
-import os
-import pickle
 import numpy as np
+from sklearn.metrics import accuracy_score
+from typing import Tuple, Callable
 
-def get_knn_model(postfix:str=None):
-    if postfix is None:
-        postfix = ""
-    else:
-        postfix = "_" + postfix
-    path = os.path.join(DATA_PATH, f"knn_model{postfix}.pkl")
-    if not os.path.exists(path):
-        make_knn_model
-    with open(path, "rb") as f:
-        knn = pickle.load(f)
-    return knn
+def make_knn_predictions(n_neighbors:int, X_train:np.ndarray, y_train:np.ndarray, X_test:np.ndarray, y_test:np.ndarray) -> Tuple[Callable, float]:
+    try:
+        import cudf, cuml
+        from cuml.neighbors import KNeighborsClassifier
+    except ImportError:
+        from sklearn.neighbors import KNeighborsClassifier
 
-def make_knn_predictions(data:np.ndarray):
-    knn = get_knn_model()
-    return knn.predict(data)
-
-def make_knn_model(n_neighbors:int, X_train:np.ndarray, y_train:np.ndarray, postfix:str=None) -> None:
-    model = cuKNeighbors(n_neighbors=n_neighbors)
-    model.fit(X_train, y_train)
-    if postfix is None:
-        postfix = ""
-    else:
-        postfix = "_" + postfix
-    with open(os.path.join(DATA_PATH, f"knn_model{postfix}.pkl"), "wb") as f:
-        pickle.dump(model, f)
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
+    acc = accuracy_score(y_test, knn.predict(X_test))
+    return knn, acc
