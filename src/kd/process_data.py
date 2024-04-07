98d2eb18147939d408d2fd038bf0a608d0b31b52
Tim Cares <peep-kestrel-0t@icloud.com>
2024-04-07 22:15:02 +0200

    Started writing PL code

--- src/kd/process_data.py
+++ src/kd/process_data.py
@@ -2,57 +2,25 @@ import sys
 sys.path.append('../fairseq/')
 sys.path.append('../')
 sys.path.append('../../')
-from typing import Dict, Any
 import json
-from collections import namedtuple
 import os
 import logging
 import hydra
 from omegaconf import OmegaConf
 from omegaconf.dictconfig import DictConfig
-from collections import OrderedDict
 import torch
-from examples.data2vec.models.data2vec2 import Data2VecMultiModel
-from examples.data2vec.models.data2vec2 import Data2VecMultiConfig
-from examples.data2vec.data.modality import Modality
-from fairseq.dataclass.utils import merge_with_parent
-from fairseq.data import Dictionary
 from datamodules import REGISTRY as DATAMODULE_REGISTRY
 from datamodules import BaseDataModule
 from rich.progress import track
+from src.utils import load_pretrained_d2v_model
 
 logger = logging.getLogger(__name__)
-
-def load_model(pretrained_model_cfg:DictConfig,
-               model_state_dict:OrderedDict[str, torch.Tensor]) -> Data2VecMultiModel:
-    
-    pretrained_model_cfg = merge_with_parent(Data2VecMultiConfig(), pretrained_model_cfg, remove_missing=True)
-
-    logger.info(f"Modality used: {pretrained_model_cfg.supported_modality}")
-    if pretrained_model_cfg.supported_modality == Modality.TEXT:
-        Task = namedtuple('Task', 'source_dictionary')
-
-        dictionary = Dictionary.load(os.path.join('..', '..', 'data', "dict.txt"))
-        dictionary.add_symbol("<mask>")
-        dummy_task = Task(source_dictionary=dictionary)
-    else:
-        dummy_task = False
-
-    model = Data2VecMultiModel.build_model(pretrained_model_cfg, task=dummy_task)
-
-    result = model.load_state_dict(model_state_dict)
-    logger.info(f'Loaded state dict, result: {result}')
-    return model
 
 
 @hydra.main(config_path=os.path.join("..", "..", "configs", "kd"), config_name="base")
 def extract_targets(cfg: DictConfig) -> None:
-    model_meta_data = torch.load(os.path.join('..', '..', 'models', cfg.model_state_dict))
-    pretrained_model_cfg = OmegaConf.create(model_meta_data['cfg']['model'])
-    model = load_model(pretrained_model_cfg=pretrained_model_cfg, model_state_dict=model_meta_data['model'])
+    model = load_pretrained_d2v_model(state_dict_path=os.path.join('..', '..', 'models', cfg.model_state_dict))
 
-    # removes decoder, and all encoders with modality != supported modality
-    model.remove_pretraining_modules(modality=pretrained_model_cfg.supported_modality)
     model.eval()
     
     datamodule_kwargs = OmegaConf.to_container(cfg.datamodule)
@@ -97,8 +65,9 @@ def extract_targets(cfg: DictConfig) -> None:
                 "indices": batch["id"].tolist(),
             })
             item = {
-                'label': pred,
+                'target': pred,
                 'source': batch['source'], # TODO: padding masks and all output dicts of datasets should have key "source" for data
+                'mode': cfg.datamodule
             }
             torch.save(item, os.path.join(kd_targets_path, filename))
 
--- src/multimodal_data2vec.py
+++ src/multimodal_data2vec.py
@@ -1,91 +1,216 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from functools import partial
-from typing import Dict, Any, List
+from typing import Dict, Any, Optional
 import numpy as np
+import os
+from utils import load_pretrained_d2v_model
+import logging
+import lightning as L
+import math
+from omegaconf import OmegaConf, II
+from dataclasses import dataclass, field
 
 from examples.data2vec.models.modalities.modules import AltBlock
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
 from examples.data2vec.models.modalities.base import MaskSeed
 
-class SimpleKDMultiModalData2Vec(nn.Module):
+logger = logging.getLogger(__name__)
+
+@dataclass
+class KDData2VecPreTrainingConfig():
+    average_top_k_layers_student:int
+    average_top_k_layers_teacher:Dict[str, int]
+    text:str = 'nlp_base.pt'
+
+    num_updates:int
+    val_frequency:int
+
+    layer_norm_target_layer:
+    instance_norm_target_layer:
+    batch_norm_target_layer:
+
+class KDData2VecPreTrainingLightningModule(L.LightningModule):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        assert self.cfg.average_top_k_layers_student <= self.cfg.model.depth,\
+            f"Can't aggregate more layers {self.cfg.average_top_k_layers_student} than available {self.cfg.model.depth}"
+        self.model = KDMMData2Vec(cfg=cfg.model)
+
+        self.average_top_k_layers_teacher: Dict[str, int] = OmegaConf.to_container(cfg.average_top_k_layers_teacher)
+
+    def forward(self, input_dict):
+        return self.model(**input_dict)
+
+    def training_step(self, batch:Dict[str, Any], batch_idx):
+        target_dict = batch.pop('target')
+        output_dict = self(**batch)
+
+        y_hat = self.make_targets(output_dict['layer_results'], num_layers=self.cfg.average_top_k_layers_student)
+
+        target = self.make_targets(target_dict['layer_results'], num_layers=self.average_top_k_layers_teacher[batch['mode']])
+
+        y_hat = y_hat[output_dict['padding_mask']]
+        target = target[target_dict['padding_mask']]
+        
+        assert y_hat.shape == target.shape() # for simple pretraining (only variant 1!) this must be the case
+        return self.kd_loss(y_hat=y_hat, y=target)
+
+    def make_targets(self, output, num_layers=None):
+        with torch.no_grad():
+            target_layer_results = output[-num_layers:]
+
+            permuted = False
+            if self.cfg.instance_norm_target_layer or self.cfg.batch_norm_target_layer:
+                target_layer_results = [
+                    tl.transpose(1, 2) for tl in target_layer_results  # BTC -> BCT
+                ]
+                permuted = True
+            if self.cfg.batch_norm_target_layer:
+                target_layer_results = [
+                    F.batch_norm(
+                        tl.float(), running_mean=None, running_var=None, training=True
+                    )
+                    for tl in target_layer_results
+                ]
+            if self.cfg.instance_norm_target_layer:
+                target_layer_results = [
+                    F.instance_norm(tl.float()) for tl in target_layer_results
+                ]
+            if permuted:
+                target_layer_results = [
+                    tl.transpose(1, 2) for tl in target_layer_results  # BCT -> BTC
+                ]
+            if self.cfg.layer_norm_target_layer:
+                target_layer_results = [
+                    F.layer_norm(tl.float(), tl.shape[-1:])
+                    for tl in target_layer_results
+                ]
+
+        output = target_layer_results[0].float()
+        for tl in target_layer_results[1:]:
+            output.add_(tl.float())
+        output = output.div_(len(target_layer_results))
+
+        if self.cfg.layer_norm_targets:
+            output = F.layer_norm(output, output.shape[-1:])
+
+        if self.cfg.instance_norm_targets:
+            output = F.instance_norm(output.transpose(1, 2)).transpose(1, 2)
+
+        return output
+    
+    def kd_loss(self, y_hat, y):
+        y_hat = y_hat.view(-1, y_hat.size(-1)).float()
+        y = y.view(-1, y_hat.size(-1))
+
+        loss = F.mse_loss(y_hat, y, reduction="none")
+
+        if self.cfg.loss_scale is not None:
+            scale = self.cfg.loss_scale
+        else:
+            scale = 1 / math.sqrt(y_hat.size(-1))
+        reg_loss = loss * scale
+
+        return reg_loss
+
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.model.parameters(), lr=0.1) # TODO
+    
+
+@dataclass
+class PretrainedStateDictsConfig():
+    audio:str = 'base_libri.pt'
+    image:str = 'base_imagenet.pt'
+    text:str = 'nlp_base.pt'
+@dataclass
+class KDMMData2VecConfig():
+    pretrained: PretrainedStateDictsConfig = PretrainedStateDictsConfig()
+
+    loss_scale: Optional[float] = field(
+        default=None,
+        metadata={
+            "help": "scale the reconstruction loss by this constant. if None then scales by 1/sqrt(dim)"
+        },
+    )
+
+    encoders_embed_dim: int = II("embed_dim") # only as a first test
+
+    depth: int = 8
+    start_drop_path_rate: float = 0
+    end_drop_path_rate: float = 0
+    num_heads: int = 12
+    norm_eps: float = 1e-6
+    norm_affine: bool = True
+    encoder_dropout: float = 0.1
+    post_mlp_drop: float = 0.1
+    attention_dropout: float = 0.1
+    activation_dropout: float = 0.0
+    dropout_input: float = 0.0
+    layerdrop: float = 0.0
+    embed_dim: int = 768
+    mlp_ratio: float = 4
+    layer_norm_first: bool = False
+
+    average_top_k_layers: int = field(
+        default=8, metadata={"help": "how many layers to average"}
+    )
+
+    end_of_block_targets: bool = False
+
+
+class KDMMData2Vec(nn.Module):
     def __init__(self,
-                 image_encoder:nn.Module,
-                 text_encoder:nn.Module,
-                 audio_encoder:nn.Module,
-                 encoders_embed_dim:int,
-                 embed_dim:int,
-                 num_heads:int,
-                 mlp_ratio:float,
-                 qkv_bias:bool,
-                 encoder_dropout:float,
-                 attention_dropout:float,
-                 activation_dropout:float,
-                 post_mlp_drop:float,
-                 norm_eps:float,
-                 norm_affine:bool,
-                 layer_norm_first:bool,
-                 ffn_targets:bool,
-                 average_top_k_layers:int,
-                 loss_beta:float,
-                 loss_scale:float,
-                 dropout_input:float,
-                 depth:int,
-                 start_drop_path_rate:float,
-                 end_drop_path_rate:float,
-                 layerdrop:float,
-                 seed:int,
+                 cfg: KDMMData2VecConfig,
                  ):
         super().__init__()
-        self.modality_encoders = nn.ModuleDict({
-            "image": image_encoder,
-            "text": text_encoder,
-            "audio": audio_encoder
-        })
+        self.cfg = cfg
+        self.modality_encoders = self._get_modality_encoders()
         self.modality_encoders.eval()
         self.modality_encoders.is_pretrained = True
 
-        if encoders_embed_dim != embed_dim:
-            self.proj = nn.Linear(encoders_embed_dim, embed_dim)
+        if self.cfg.encoders_embed_dim != self.cfg.embed_dim:
+            self.proj = nn.Linear(self.cfg.encoders_embed_dim, self.cfg.embed_dim)
 
         make_layer_norm = partial(
-            nn.LayerNorm, eps=norm_eps, elementwise_affine=norm_affine
+            nn.LayerNorm, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine
         )
 
         def make_block(drop_path, dim=None, heads=None):
             return AltBlock(
-                dim=embed_dim if dim is None else dim,
-                num_heads=num_heads if heads is None else heads,
-                mlp_ratio=mlp_ratio,
-                qkv_bias=qkv_bias,
-                drop=encoder_dropout,
-                attn_drop=attention_dropout,
-                mlp_drop=activation_dropout,
-                post_mlp_drop=post_mlp_drop,
+                dim=self.cfg.embed_dim if dim is None else dim,
+                num_heads=self.cfg.num_heads if heads is None else heads,
+                mlp_ratio=self.cfg.mlp_ratio,
+                qkv_bias=True,
+                drop=self.cfg.encoder_dropout,
+                attn_drop=self.cfg.attention_dropout,
+                mlp_drop=self.cfg.activation_dropout,
+                post_mlp_drop=self.cfg.post_mlp_drop,
                 drop_path=drop_path,
                 norm_layer=make_layer_norm,
-                layer_norm_first=layer_norm_first,
-                ffn_targets=ffn_targets,
+                layer_norm_first=self.cfg.layer_norm_first,
+                ffn_targets=not self.cfg.end_of_block_targets,
             )
         
         self.alibi_biases = {}
 
-        self.average_top_k_layers = average_top_k_layers
-        self.loss_beta = loss_beta
-        self.loss_scale = loss_scale
+        self.loss_scale = self.cfg.loss_scale
 
-        self.dropout_input = nn.Dropout(dropout_input)
+        self.dropout_input = nn.Dropout(self.cfg.dropout_input)
 
-        dpr = np.linspace(start_drop_path_rate, end_drop_path_rate, depth)
+        dpr = np.linspace(self.cfg.start_drop_path_rate, self.cfg.end_drop_path_rate, self.cfg.depth)
 
-        self.blocks = nn.ModuleList([make_block(dpr[i]) for i in range(depth)])
+        self.blocks = nn.ModuleList([make_block(dpr[i]) for i in range(self.cfg.depth)])
 
         self.norm = None
-        if layer_norm_first:
-            self.norm = make_layer_norm(embed_dim)
+        if self.cfg.layer_norm_first:
+            self.norm = make_layer_norm(self.cfg.embed_dim)
 
-        self.layerdrop = layerdrop
-        self.seed = seed
+        self.layerdrop = self.cfg.layerdrop
+        self.seed = self.cfg.seed
 
         self.apply(self._init_except_pretrained)
 
@@ -94,6 +219,21 @@ class SimpleKDMultiModalData2Vec(nn.Module):
                 p.optim_overrides = {"optimizer": {"weight_decay_scale": 0}}
 
         self.num_updates = 0
+
+    def _get_modality_encoders(self) -> None:
+        modality_encoders = {}
+        embed_dims = []
+        for mode, state_dict_name in self.cfg.pretrained.items():
+            logger.info(f'Loading modality encoder for: {mode}')
+            state_dict_path = os.path.join('..', 'models', state_dict_name)
+            embed_dim = torch.load(state_dict_path)['cfg']['model']['embed_dim']
+            embed_dims.append(embed_dim)
+            d2v_model = load_pretrained_d2v_model(state_dict_path=state_dict_path)
+            mode_feature_extractor = d2v_model.modality_encoders[mode]
+            modality_encoders[mode] = mode_feature_extractor
+        assert len(set(embed_dims)) == 1, f'Embedding dimensions of modality encoders do not align: {embed_dims}'
+
+        return nn.ModuleDict(modality_encoders)
 
     def _init_except_pretrained(self, module:nn.Module):
         if hasattr(module, "is_pretrained") and module.is_pretrained:
@@ -106,10 +246,8 @@ class SimpleKDMultiModalData2Vec(nn.Module):
 
     def forward(
         self,
-        image,
-        text,
-        audio,
-        target=None,
+        source,
+        mode,
         id=None,
         padding_mask=None,
         mask=True,
@@ -123,31 +261,20 @@ class SimpleKDMultiModalData2Vec(nn.Module):
         if id is not None:
             mask_seeds = MaskSeed(seed=self.seed, update=self.num_updates, ids=id)
         
-        extractor_outputs = []
-        for mode, input in zip(["image", "text", "audio"], [image, text, audio]):
-            if input is not None:
-                feature_extractor = self.modality_encoders[mode]
-                with torch.no_grad():
-                    extractor_out = feature_extractor(
-                        input,
-                        padding_mask,
-                        mask,
-                        remove_masked=not features_only or force_remove_masked,
-                        clone_batch=self.cfg.clone_batch if not features_only else 1,
-                        mask_seeds=mask_seeds,
-                        precomputed_mask=precomputed_mask,
-                    )
+        feature_extractor = self.modality_encoders[mode]
+        with torch.no_grad():
+            extractor_out = feature_extractor(
+                source,
+                padding_mask,
+                mask,
+                remove_masked=not features_only or force_remove_masked,
+                clone_batch=self.cfg.clone_batch if not features_only else 1,
+                mask_seeds=mask_seeds,
+                precomputed_mask=precomputed_mask,
+            )
 
-                if self.proj is not None:
-                    extractor_out = self.proj(extractor_out)
-                extractor_outputs.append(extractor_out)
-
-        x = extractor_outputs[0]
-
-        # if len(extractor_outputs) == 1:        
-        #     x = extractor_outputs[0]
-        # else:
-        #     x = torch.cat(extractor_outputs, dim=1) # TODO: look into it later when input is multimodal...
+            if self.proj is not None:
+                extractor_out = self.proj(extractor_out)
 
         x = extractor_out["x"]
         encoder_mask = extractor_out["encoder_mask"]
@@ -198,3 +325,35 @@ class SimpleKDMultiModalData2Vec(nn.Module):
             "layer_results": layer_results,
             "mask": encoder_mask,
         }
+
+    
+
+def build_simple_kd_mm_d2v(cfg) -> KDMMData2Vec:
+    pass
+    
+
+# extractor_outputs = []
+# for mode, input in zip(["image", "text", "audio"], [image, text, audio]):
+#     if input is not None:
+#         feature_extractor = self.modality_encoders[mode]
+#         with torch.no_grad():
+#             extractor_out = feature_extractor(
+#                 input,
+#                 padding_mask,
+#                 mask,
+#                 remove_masked=not features_only or force_remove_masked,
+#                 clone_batch=self.cfg.clone_batch if not features_only else 1,
+#                 mask_seeds=mask_seeds,
+#                 precomputed_mask=precomputed_mask,
+#             )
+# 
+#         if self.proj is not None:
+#             extractor_out = self.proj(extractor_out)
+#         extractor_outputs.append(extractor_out)
+# 
+# x = extractor_outputs[0]
+# 
+# if len(extractor_outputs) == 1:        
+#     x = extractor_outputs[0]
+# else:
+#     x = torch.cat(extractor_outputs, dim=1) # TODO: look into it later when input is multimodal...
