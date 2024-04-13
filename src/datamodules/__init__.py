78edc67f0fbbcfc67211e50d616e714981739b5b
Tim Cares <peep-kestrel-0t@icloud.com>
2024-04-13 23:08:57 +0200

    Adjustment in imports

--- src/datamodules/__init__.py
+++ src/datamodules/__init__.py
@@ -1,5 +1,5 @@
-from .multimodal_datamodules import *
 from .unimodal_datamodules import *
+from .multimodal_datamodules import *
 from .kd_datamodules import *
 
 REGISTRY = UNIMODAL_REGISTRY | MULTIMODAL_REGISTRY | KD_DATAMODULE_REGISTRY # combine them
