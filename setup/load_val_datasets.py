48e9f89c47bcc0509b4eacea5799db8fd401d7db
Tim Cares <peep-kestrel-0t@icloud.com>
2024-04-19 14:16:57 +0200

    Added val data loading script for simplicity

---
+++ setup/load_val_datasets.py
@@ -0,0 +1,30 @@
+import hydra
+from omegaconf import OmegaConf, open_dict, DictConfig
+import os
+import logging
+
+from torchaudio.datasets import LIBRISPEECH, SPEECHCOMMANDS
+from torchvision.datasets import CIFAR10, CIFAR100
+
+logger = logging.getLogger(__name__)
+
+@hydra.main(version_base=None, config_path=os.path.join("..", "configs", "training"))
+def main(cfg:DictConfig) -> None:
+    data_path = cfg.data_path
+
+    for url in ["test-other", "train-clean-100", "train-clean-360"]:
+        LIBRISPEECH(root=data_path, url=url, download=True) # no val set, but simple to just download here
+
+    for subset in ["training", "testing"]:
+        SPEECHCOMMANDS(data_path, subset=subset, download=True)
+
+
+    imdb_path = os.path.join(data_path, 'language', 'imdb')
+    for subset in ["train", "test"]:
+        CIFAR10(data_path, train=subset, download=True)
+        CIFAR100(data_path, train=subset, download=True)
+
+        # IMDB(root=imdb_path, split=subset) -> done during setup of datamodules -> fits better there
+
+if __name__ == "__main__":
+    main()
