8868254062ba1ff79a65a551b572f50d46b28a38
Tim Cares <peep-kestrel-0t@icloud.com>
2024-05-01 14:31:37 +0200

    Added logging of avg. gpu time per step

--- src/callbacks/utils.py
+++ src/callbacks/utils.py
@@ -9,6 +9,7 @@ class WallClockCallback(Callback):
     def __init__(self):
         self.gpu_wall_clock_time = 0.0
         self.start_time = None
+        self.n_batches = 0
 
     def on_train_batch_start(self, trainer:Trainer, pl_module:LightningModule, batch:Any, batch_idx:int) -> None:
         self.start_time = time()
@@ -17,8 +18,12 @@ class WallClockCallback(Callback):
                            outputs, batch: Any, batch_idx: int) -> None:
         elapsed_time = time() - self.start_time
         self.gpu_wall_clock_time += elapsed_time
+        self.n_batches += 1
 
     def on_train_end(self, trainer:Trainer, pl_module:LightningModule) -> None:
         logger.info(f"GPU Wall Clock Time: {self.gpu_wall_clock_time/60:.2f} minutes")
         logger.info(f"GPU Wall Clock Time: {self.gpu_wall_clock_time/3600:.2f} hours")
         trainer.logger.experiment.log({"gpu_wall_clock_time": self.gpu_wall_clock_time})
+
+        logger.info(f"Average GPU Wall Clock Time per batch: {self.gpu_wall_clock_time/self.n_batches:.2f} seconds")
+        trainer.logger.experiment.log({"gpu_wall_clock_time_per_batch": self.gpu_wall_clock_time/self.n_batches})
