d18ec1974d7925034e78d47cbca4061a77956fa0
Tim Cares <peep-kestrel-0t@icloud.com>
2024-05-08 14:57:31 +0200

    Moved saliency masking to utils

--- src/multimodal_data2vec.py
+++ src/multimodal_data2vec.py
@@ -18,24 +18,10 @@ import contextlib
 
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
 from data2vec_fairseq.models.modalities.modules import AltBlock
+from utils import prepare_output, get_max_saliency_patches, prepare_salient_patches
 
 logger = logging.getLogger(__name__)
 
-def prepare_output(out:List[torch.Tensor], modality:Modality, norm:bool=True) -> List[torch.Tensor]:
-    if norm:
-        out = [
-            F.instance_norm(tl.transpose(1, 2).float()).transpose(1, 2)
-            for tl in out  # BTC -> BCT -> BTC
-        ]
-
-    y = out[0].float()
-    for tl in out[1:]:
-        y.add_(tl.float())
-    y = y.div_(len(out))
-
-    if modality == Modality.IMAGE:
-        y = F.layer_norm(y, y.shape[-1:])
-    return y
 
 class KDData2VecPreTrainingLightningModule(L.LightningModule):
     def __init__(self, cfg):
@@ -44,6 +30,7 @@ class KDData2VecPreTrainingLightningModule(L.LightningModule):
 
         self.d2v_masking = self.cfg.model.mask and self.cfg.model.d2v_masking
         self.masked_kd = self.cfg.model.mask and not self.cfg.model.d2v_masking
+        self.inverse_masked_kd = self.masked_kd and self.cfg.model.inverse_masked_kd
 
         state_dict_name = self.cfg.model.pretrained['image']
         state_dict_path = os.path.join(self.cfg.model.pretrained_path, state_dict_name)
@@ -65,7 +52,11 @@ class KDData2VecPreTrainingLightningModule(L.LightningModule):
         self.save_hyperparameters()
 
     def forward(self, input_dict):
-        return self.model(**input_dict, features_only=False, return_encoder_output=True)
+        return self.model(**input_dict,
+                          features_only=False,
+                          return_encoder_output=True,
+                          unmasked_feature_extractor_only=self.inverse_masked_kd)
+        # "unmasked_feature_extractor_only" has higher precedence than "features_only" and "return_encoder_output"
 
     def training_step(self, batch:Dict[str, Any], batch_idx):
         if 'target' in batch:
@@ -308,6 +299,7 @@ class KDMMData2Vec(nn.Module):
         precomputed_mask=None,
         return_encoder_output:bool=False,
         features_only:bool=False,
+        unmasked_feature_extractor_only:bool=False,
     ):
         assert len(modes)==1, f"This model accepts exactly modality indicator at a time, received: {modes}"
         n_sources = sum(mode is not None for mode in [audio, image, text])
@@ -332,13 +324,18 @@ class KDMMData2Vec(nn.Module):
 
         feature_extractor:ModalitySpecificEncoder = self.modality_encoders[mode]
         with torch.no_grad() if not self.fine_tuning else contextlib.ExitStack():
-            x_local = feature_extractor.local_features(source) # if we do masking, we reuse the local features
+            x_local = feature_extractor.local_features(source) # if we do d2v masking, we reuse the local features
             extractor_out_unmasked = feature_extractor.contextualized_features(
                 x=x_local,
                 padding_mask=padding_mask,
                 mask=False,
                 remove_masked=False,
             )
+            if unmasked_feature_extractor_only:
+                # we return a dict here, as we do not need to change the "training_step" function of the PL module that extensive
+                return {
+                    "encoder_output": extractor_out_unmasked
+                }
             
             if d2v_masking:
                 extractor_out_masked = feature_extractor.contextualized_features(
@@ -401,7 +398,8 @@ class KDMMData2Vec(nn.Module):
 
         if masked_kd:
             assert not d2v_masking, "MaskedKD and d2v masking are mutually exclusive."
-            x_unmasked_tokens_only, keep_timesteps = self.get_max_saliency_patches(
+            x_unmasked_tokens_only, keep_timesteps = get_max_saliency_patches(
+                frac_keep_tokens=self.cfg.frac_keep_tokens,
                 attn_results=attn_results,
                 extractor_out=extractor_out, # in this case must be "extractor_out_unmasked" (d2v_masking=False)
                 feature_extractor=feature_extractor,
@@ -409,11 +407,12 @@ class KDMMData2Vec(nn.Module):
             
             extractor_out_unmasked['x'] = x_unmasked_tokens_only
 
-            layer_results = self.prepare_salient_patches(
-                    layer_results=layer_results,
-                    keep_timesteps=keep_timesteps,
-                    mode=modes[0],
-                    )
+            layer_results = prepare_salient_patches(
+                layer_results=layer_results,
+                keep_timesteps=keep_timesteps,
+                mode=modes[0],
+                norm_first=self.cfg.norm_first,
+            )
 
         if self.norm is not None:
             x = self.norm(x)
@@ -473,87 +472,6 @@ class KDMMData2Vec(nn.Module):
         x = decoder(*x)
 
         return x
-    
-    def get_max_saliency_patches(self,
-                                 attn_results:List[torch.Tensor],
-                                 extractor_out:Dict[str, torch.Tensor],
-                                 feature_extractor:ModalitySpecificEncoder,
-                                 ) -> Tuple[torch.Tensor, torch.Tensor]:
-        
-        alibi_scale = extractor_out.get("alibi_scale", None)
-        x_unmasked = extractor_out['x_pre_context'] # will definitely be unmasked, as "d2v_masking" is False
-        masked_padding_mask = extractor_out["padding_mask"]
-        masked_alibi_bias = extractor_out.get("alibi_bias", None)
-        
-        # TODO: think about option for audio -> no special token at the beginning
-        attn_score = attn_results[0].float()
-        for tl in attn_results[1:]:
-            attn_score.add_(tl.float())
-        attn_score = attn_score.div_(len(attn_results))
-
-        ### partly adapted from: https://arxiv.org/pdf/2302.10494 (MaskedKD) ###
-        num_keep = int(self.cfg.frac_keep_tokens*attn_score.size(-1)) # or size(-2) because shape is (B, H, T, T)
-        keep_timesteps = torch.topk(attn_score.mean(dim=1)[:, 0, 1:], num_keep).indices # attn_score.mean(dim=1): B x H x T x T -> B x T x T
-
-        # pre context already contains special token at the beginning
-        cls_save = x_unmasked[:, 0, :].unsqueeze(dim=1) # B x 1 x D
-        x_unmasked = x_unmasked[:, 1:, :]
-
-        index = keep_timesteps.unsqueeze(-1).repeat(1, 1, self.cfg.embed_dim)
-        x_unmasked_tokens_only = torch.gather(x_unmasked, dim=1, index=index)
-        x_unmasked_tokens_only = torch.cat((cls_save , x_unmasked_tokens_only), dim=1)
-        # B x num_keep+1 x D -> one (+1) stems from additional special token
-
-        ### end of adaptation ###
-
-        if masked_padding_mask is not None:
-            padding_masked_unmasked_tokens = torch.gather(masked_padding_mask[:, 1:], dim=1, index=keep_timesteps)
-            # the following should never raise and exception, as long as "num_keep" is not larger than the number of
-            # non-padding tokens in the input, this is because padded tokens have an attention score of 0, which is the minimum
-            assert (~padding_masked_unmasked_tokens).all(), "All non-masked MaskedKD tokens should be padding tokens."
-
-        # now compute modality encoder output for the teacher model, we mask the tokens before the
-        # context encoder of the modality encoder -> same approach as d2v masking
-        # we reuse the features before the context encoder, saved from the forward pass of the modality encoder at the beginning
-        x_unmasked_tokens_only = feature_extractor.context_encoder(
-            x_unmasked_tokens_only, # our "x" here
-            masked_padding_mask, # masked padding mask can be reused from previous modality encoder forward pass
-            masked_alibi_bias, # alibi can be reused from previous modality encoder forward pass
-            alibi_scale[: feature_extractor.modality_cfg.prenet_depth]
-            if alibi_scale is not None
-            else None,
-        )
-        return x_unmasked_tokens_only, keep_timesteps
-
-    def prepare_salient_patches(self,
-                                layer_results:List[torch.Tensor],
-                                keep_timesteps:torch.Tensor,
-                                mode:Modality,
-                                ) -> torch.Tensor:
-        # if final_attn_layer_saliency_score is True, then "layer_results" only contains one element, the last layer
-        # ... but it can be treated the same way as if it contains all layers
-
-        if self.cfg.norm_first: # norms over all time steps, including those that are not used for the teacher
-            layer_results = [F.instance_norm(tl.transpose(1, 2).float()).transpose(1, 2) for tl in layer_results]
-        
-        layer_results = torch.stack(layer_results)
-        cls_save = layer_results[:, :, 0, :].unsqueeze(dim=2) # L x B x 1 x D
-        layer_results = layer_results[:, :, 1:, :]
-
-        # add dim for embedding dimension (-1) and for layer dimension (0)
-        n_layers = self.cfg.depth if not self.cfg.final_attn_layer_saliency_score else 1
-        index = keep_timesteps.unsqueeze(-1).unsqueeze(0).repeat(n_layers, 1, 1, self.cfg.embed_dim)
-        layer_results = torch.gather(layer_results, dim=2, index=index)
-        layer_results = torch.cat((cls_save , layer_results), dim=2)
-        # "layer_results" now only consists of same tokens as "x_unmasked_tokens_only"
-
-        # averaged and normed layer results only on unmasked tokens
-        # -> teacher only gets the unmasked tokens, and the teacher output is normed,
-        # so we need to norm only the unmasked tokens for the student output as well
-        layer_results = [layer_results[i] for i in range(len(layer_results))] # expand to list
-        layer_results = prepare_output(out=layer_results, modality=mode, norm=not self.cfg.norm_first)
-        # B x num_keep+1 x D -> one (+1) stems from additional special token
-        return layer_results
     
 
     def extract_features(
--- src/utils.py
+++ src/utils.py
@@ -3,18 +3,20 @@ import os
 import torch
 import logging
 from collections import namedtuple
-import os
 import logging
-from omegaconf import OmegaConf, open_dict
+from omegaconf import OmegaConf
 from omegaconf.dictconfig import DictConfig
 from collections import OrderedDict
-from dataclasses import is_dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from data2vec_fairseq.models.data2vec2 import Data2VecMultiModel
 from data2vec_fairseq.models.data2vec2 import Data2VecMultiConfig
+from data2vec_fairseq.models.modalities.base import ModalitySpecificEncoder
 from fairseq.data import Dictionary
 from fairseq.dataclass.utils import merge_with_parent
+
+from datasets_.base_datasets import Modality
+import torch.nn.functional as F
 
 logger = logging.getLogger(__name__)
 
@@ -71,3 +73,106 @@ def pad_text_sequence(tokens:List[int],
     language_tokens =  tokens + [pad_idx] * (num_max_bpe_tokens - num_tokens)
 
     return language_tokens, padding_mask
+
+
+def prepare_output(out:List[torch.Tensor], modality:Modality, norm:bool=True) -> List[torch.Tensor]:
+    if norm:
+        out = [
+            F.instance_norm(tl.transpose(1, 2).float()).transpose(1, 2)
+            for tl in out  # BTC -> BCT -> BTC
+        ]
+
+    y = out[0].float()
+    for tl in out[1:]:
+        y.add_(tl.float())
+    y = y.div_(len(out))
+
+    if modality == Modality.IMAGE:
+        y = F.layer_norm(y, y.shape[-1:])
+    return y
+
+
+def get_max_saliency_patches(
+        frac_keep_tokens:float,
+        attn_results:List[torch.Tensor],
+        extractor_out:Dict[str, torch.Tensor],
+        feature_extractor:ModalitySpecificEncoder,
+        ) -> Tuple[torch.Tensor, torch.Tensor]:
+        
+    alibi_scale = extractor_out.get("alibi_scale", None)
+    x_unmasked = extractor_out['x_pre_context'] # will definitely be unmasked, as "d2v_masking" is False
+    masked_padding_mask = extractor_out["padding_mask"]
+    masked_alibi_bias = extractor_out.get("alibi_bias", None)
+    
+    # TODO: think about option for audio -> no special token at the beginning
+    attn_score = attn_results[0].float()
+    for tl in attn_results[1:]:
+        attn_score.add_(tl.float())
+    attn_score = attn_score.div_(len(attn_results))
+
+    ### partly adapted from: https://arxiv.org/pdf/2302.10494 (MaskedKD) ###
+    num_keep = int(frac_keep_tokens*attn_score.size(-1)) # or size(-2) because shape is (B, H, T, T)
+    keep_timesteps = torch.topk(attn_score.mean(dim=1)[:, 0, 1:], num_keep).indices # attn_score.mean(dim=1): B x H x T x T -> B x T x T
+
+    # pre context already contains special token at the beginning
+    cls_save = x_unmasked[:, 0, :].unsqueeze(dim=1) # B x 1 x D
+    x_unmasked = x_unmasked[:, 1:, :]
+
+    embed_dim = x_unmasked.size(-1)
+    index = keep_timesteps.unsqueeze(-1).repeat(1, 1, embed_dim)
+    x_unmasked_tokens_only = torch.gather(x_unmasked, dim=1, index=index)
+    x_unmasked_tokens_only = torch.cat((cls_save , x_unmasked_tokens_only), dim=1)
+    # B x num_keep+1 x D -> one (+1) stems from additional special token
+
+    ### end of adaptation ###
+
+    if masked_padding_mask is not None:
+        padding_masked_unmasked_tokens = torch.gather(masked_padding_mask[:, 1:], dim=1, index=keep_timesteps)
+        # the following should never raise and exception, as long as "num_keep" is not larger than the number of
+        # non-padding tokens in the input, this is because padded tokens have an attention score of 0, which is the minimum
+        assert (~padding_masked_unmasked_tokens).all(), "All non-masked MaskedKD tokens should be padding tokens."
+
+    # now compute modality encoder output for the teacher model, we mask the tokens before the
+    # context encoder of the modality encoder -> same approach as d2v masking
+    # we reuse the features before the context encoder, saved from the forward pass of the modality encoder at the beginning
+    x_unmasked_tokens_only = feature_extractor.context_encoder(
+        x_unmasked_tokens_only, # our "x" here
+        masked_padding_mask, # masked padding mask can be reused from previous modality encoder forward pass
+        masked_alibi_bias, # alibi can be reused from previous modality encoder forward pass
+        alibi_scale[: feature_extractor.modality_cfg.prenet_depth]
+        if alibi_scale is not None
+        else None,
+    )
+    return x_unmasked_tokens_only, keep_timesteps
+
+def prepare_salient_patches(
+        layer_results:List[torch.Tensor],
+        keep_timesteps:torch.Tensor,
+        mode:Modality,
+        norm_first:bool,
+        ) -> torch.Tensor:
+    # if final_attn_layer_saliency_score is True, then "layer_results" only contains one element, the last layer
+    # ... but it can be treated the same way as if it contains all layers
+
+    if norm_first: # norms over all time steps, including those that are not used for the teacher
+        layer_results = [F.instance_norm(tl.transpose(1, 2).float()).transpose(1, 2) for tl in layer_results]
+    
+    layer_results = torch.stack(layer_results)
+    cls_save = layer_results[:, :, 0, :].unsqueeze(dim=2) # L x B x 1 x D
+    layer_results = layer_results[:, :, 1:, :]
+
+    embed_dim = layer_results.size(-1)
+    # add dim for embedding dimension (-1) and for layer dimension (0)
+    n_layers = layer_results.size(0)
+    index = keep_timesteps.unsqueeze(-1).unsqueeze(0).repeat(n_layers, 1, 1, embed_dim)
+    layer_results = torch.gather(layer_results, dim=2, index=index)
+    layer_results = torch.cat((cls_save , layer_results), dim=2)
+    # "layer_results" now only consists of same tokens as "x_unmasked_tokens_only"
+
+    # averaged and normed layer results only on unmasked tokens
+    # -> teacher only gets the unmasked tokens, and the teacher output is normed,
+    # so we need to norm only the unmasked tokens for the student output as well
+    layer_results = [layer_results[i] for i in range(len(layer_results))] # expand to list
+    layer_results = prepare_output(out=layer_results, modality=mode, norm=not norm_first)
+    # B x num_keep+1 x D -> one (+1) stems from additional special token
+    return layer_results
