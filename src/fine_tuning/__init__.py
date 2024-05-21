127be3da96eaeb0b02bed14978b18394a9d7b628
Tim Cares <peep-kestrel-0t@icloud.com>
2024-05-21 15:01:32 +0200

    Fixed import

--- src/fine_tuning/__init__.py
+++ src/fine_tuning/__init__.py
@@ -2,3 +2,7 @@ from .image_classification import (
     ImageClassificationConfig,
     ImageClassificationLightningModule
 )
+from .text_classification import (
+    TextClassificationConfig,
+    TextClassificationLightningModule
+)
