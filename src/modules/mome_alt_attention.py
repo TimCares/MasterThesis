f19d5f10ed778829e72106a06f8844ff6372db08
Tim Cares <peep-kestrel-0t@icloud.com>
2024-05-25 00:06:18 +0200

    Modified MoME module

--- src/modules/mome_alt_attention.py
+++ src/modules/mome_alt_attention.py
@@ -1,6 +1,8 @@
+import torch
 import torch.nn as nn
 from data2vec_fairseq.models.modalities.modules import AltAttention, AltBlock
 import logging
+from timm.models.vision_transformer import DropPath, Mlp
 
 logger = logging.getLogger(__name__)
 
@@ -22,17 +24,21 @@ class MOMEAltBlock(nn.Module):
         act_layer=nn.GELU,
         norm_layer=nn.LayerNorm,
         layer_norm_first=True,
-        ffn_targets=False,
         cosine_attention=False,
+        num_max_nlp_tokens=512,
+        with_vl=False,
     ):
         super().__init__()
 
         self.layer_norm_first = layer_norm_first
-        self.ffn_targets = ffn_targets
+        self.num_max_nlp_tokens = num_max_nlp_tokens
+        self.with_vl = with_vl
 
-        from timm.models.vision_transformer import DropPath, Mlp
+        if self.with_vl:
+            self.experts = ['vl']
+        else:
+            self.experts = ['image', 'text']
 
-        self.norm1 = norm_layer(dim)
         self.attn = AltAttention(
             dim,
             num_heads=num_heads,
@@ -44,12 +50,8 @@ class MOMEAltBlock(nn.Module):
         )
 
         self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
-        self.norm2 = nn.ModuleDict({
-            'audio': norm_layer(dim),
-            'image': norm_layer(dim),
-            'text': norm_layer(dim),
-            # 'all': norm_layer(dim),
-        })
+        self.norm1 = nn.ModuleDict({modality: norm_layer(dim) for modality in self.experts})
+        self.norm2 = nn.ModuleDict({modality: norm_layer(dim) for modality in self.experts})
 
         mlp_hidden_dim = int(dim * mlp_ratio)
         def make_mlp():
@@ -59,49 +61,47 @@ class MOMEAltBlock(nn.Module):
                 act_layer=act_layer,
                 drop=mlp_drop,
             )
-        self.mlp = nn.ModuleDict({
-            'audio': make_mlp(),
-            'image': make_mlp(),
-            'text': make_mlp(),
-            # 'all': make_mlp(),
-        })
+        self.mlp = nn.ModuleDict({modality: make_mlp() for modality in self.experts})
 
-        self.post_mlp_dropout = nn.ModuleDict({
-            'audio': nn.Dropout(post_mlp_drop, inplace=False),
-            'image': nn.Dropout(post_mlp_drop, inplace=False),
-            'text': nn.Dropout(post_mlp_drop, inplace=False),
-            # 'all': nn.Dropout(post_mlp_drop, inplace=False),
-        })
+        self.post_mlp_dropout = nn.ModuleDict({modality: nn.Dropout(post_mlp_drop) for modality in self.experts})
 
         self.attention_pretrained = False
 
-    def forward(self, x, mode:str, padding_mask=None, alibi_bias=None):
-        if self.layer_norm_first:
-            x = x + self.drop_path(self.attn(self.norm1(x), padding_mask, alibi_bias))
-            r = x = self.mlp[mode](self.norm2[mode](x))
+    def forward(self, x, modality:str, padding_mask=None, alibi_bias=None):
+        x = x + self.drop_path(self.attn(x, padding_mask, alibi_bias))
+
+        if modality in ['image', 'text'] or self.with_vl:
+            r = x = self.norm1[modality](x)
+            x = self.mlp[modality](x)
             t = x
-            x = r + self.drop_path(self.post_mlp_dropout[mode](x))
-            if not self.ffn_targets:
-                t = x
+            x = self.norm2[modality](r + self.drop_path(self.post_mlp_dropout[modality](x)))
         else:
-            x = x + self.drop_path(self.attn(x, padding_mask, alibi_bias))
-            r = x = self.norm1(x)
-            x = self.mlp[mode](x)
-            t = x
-            x = self.norm2[mode](r + self.drop_path(self.post_mlp_dropout[mode](x)))
-            if not self.ffn_targets:
-                t = x
+            x_text = x[:, : self.num_max_nlp_tokens]
+            x_image = x[:, self.num_max_nlp_tokens :]
+            mlp_result = []
+            x = []
+            for x_modality in [x_text, x_image]:
+                r = x_modality = self.norm1[modality](x_modality)
+                x_modality = self.mlp[modality](x_modality)
+                t = x_modality
+                x_modality = self.norm2[modality](r + self.drop_path(self.post_mlp_dropout[modality](x_modality)))
+
+                mlp_result.append(t)
+                x.append(x_modality)
+            
+            t = torch.cat(mlp_result, dim=1)
+            x = torch.cat(x, dim=1)
 
         return x, t
     
     def init_from_pretrained(self, pretained_block:AltBlock, mode:str, init_attention:bool) -> None:
+        self.norm1[mode] = pretained_block.norm1
         self.norm2[mode] = pretained_block.norm2
         self.mlp[mode] = pretained_block.mlp
         self.post_mlp_dropout[mode] = pretained_block.post_mlp_dropout
 
         if init_attention and not self.attention_pretrained:
             self.attn = pretained_block.attn
-            self.norm1 = pretained_block.norm1
             self.attention_pretrained = True
         elif init_attention and self.attention_pretrained:
             logger.warning("Attention already initialized from pretrained block, not reinitializing")
