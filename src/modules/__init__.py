65afa8593c0c75abab1d26179086d9113b8d8029
Tim Cares <peep-kestrel-0t@icloud.com>
2024-06-17 00:33:13 +0200

    Fixed import error

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,3 +1,3 @@
 from .block_targets import LayerResultBlock
 from .mome import MOMEAltBlock, MOMEBlock
-from .contrastive_learning import ContrastiveLearningModule
+from .contrastive_learning import ContrastiveLearningMemoryBankModule
