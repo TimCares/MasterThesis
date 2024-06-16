77781f05baa733949a4bcde0745dcf0e19e74f9f
Tim Cares <peep-kestrel-0t@icloud.com>
2024-06-17 00:16:45 +0200

    Switched grad acc module for contrastive learning to (decaying) memory bank module for contrastive learning

--- docs/thesis/experiments/multimodal_knowledge_distillation.typ
+++ docs/thesis/experiments/multimodal_knowledge_distillation.typ
@@ -3,6 +3,18 @@
 
 === Baseline
 
+- currently only 6 layers, 5 out of which are modality specific, 1 is shared
+- we experiment with adding one additional moadality specific layer, and one additional shared layer in another experiment
+-> more difficult to align mutliple modalities, than just training one -> add one layer
+  -> motivation for modality specific: after 5 layers information might not be high level enough so that one layer can process the information
+    -> add one additional modality specific
+  -> motivation for shared: after 5 layers information might be high level enough, but capturing modality agnostic information might take more than one layer
+    -> add one additional shared
+
+- added shared layer improves performance slightly, but adds 7 million parameters and 41 minutes to training time
+- looking at the improvement in zero-shot, which increases the average Recall from 29.93% to 30.8%, this is not much of an improvement, 
+  considering the amount of parameters we add to the model
+
 #figure(
   table(
   columns: (25%, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto, auto),
@@ -69,3 +81,44 @@
   ),
   caption: [],
 )<image_text_retrieval_2>
+
+- looking at the validation loss of image and text seperatly, on COCO val set, we observe that the loss on images is
+  significantly lower than the loss on text, which might be due to the fact that the teacher model is a vision model and
+  the target, the cls token, might be biased towards the image modality, as it is unimodal
+- interestingly, this bias also seem to be directly translated to the performance on image-text retrieval, as the performance
+  on image-text retrieval is significantly higher than on text-image retrieval
+  -> we are learning the cls token representation, and using the leared cls token as an output for the student model,
+    to encode a modality for retrieval and other downstream task
+-> suggests that the cls token is biased towards the image modality, or rather that the model is better in encoding images than
+  text
+- we can see that the performance of e.g. BEiT-3 and VLMo is also lower on text-image retrieval than on image-text retrieval,
+  but not the the extend that we observe with our model
+
+#figure(
+  table(
+  columns: (25%, auto, auto, auto, auto),
+    stroke: none,
+    table.hline(),
+    table.header(
+      table.cell(rowspan: 2, colspan: 1, align:horizon, [*Model*]),
+      table.cell(colspan: 2, [*MSCOCO (5K test set)*]),
+      table.cell(colspan: 2, [*Flickr30K (1K test set)*]),
+      table.cell(colspan: 1, [Image $arrow.r$ Text]),
+      table.cell(colspan: 1, [Text $arrow.r$ Image]),
+      table.vline(stroke: .4pt),
+      table.cell(colspan: 1, [Image $arrow.r$ Text]),
+      table.cell(colspan: 1, [Text $arrow.r$ Image]),
+    ),
+    table.hline(stroke: .4pt),
+    [*MM-D2V2 7(Ours)†*], [51.39], [28.11], [23.46], [14.71],
+    [BEiT-3], [93.2], [82.57], [99.33], [96.17],
+    [VLMo], [88.27], [76.53], [97.2], [90.93],
+    table.hline(),
+  ),
+  caption: [Average recall of image-text and text-image retrieval on MSCOCO and Flickr30K. All models continously perform better on
+  image-text retrieval than on text-image retrieval, but the difference is more pronounced for our model.],
+)<image_text_retrieval_mean>
+
+- currently vl layer(s) (or rather mulimodal layer(s)) are randomly initialized, one option is to specifically initialize the multimodal
+  layers with the weight of the final layers of the D2V text model
+  -> initial state is closer closer to text modality
--- src/modules/contrastive_learning.py
+++ src/modules/contrastive_learning.py
@@ -3,21 +3,56 @@ import torch.nn.functional as F
 from typing import Tuple
 
 
-class ContrastiveLearningModule:
-    def __init__(self):
+class ContrastiveLearningMemoryBankModule:
+    def __init__(
+            self,
+            embed_size:int=768,
+            batch_size:int=256,
+            size:int=16384, # 2^14
+            decay_rate:float=0.99,
+            schedule_decay_rate:bool=False): # decay rate of 1 means no decay -> all samples are weighted equally
         super().__init__()
-        self.acc_img_emb = []
-        self.acc_text_emb = []
+        self.batch_size = batch_size
+        self.size = size - batch_size # one batch is always new samples
+        self.decay_rate = decay_rate
+        self.schedule_decay_rate = schedule_decay_rate
+        assert size % batch_size == 0, "Size of memory bank must be multiple of batch size"
+        self.image_memory_bank = torch.zeros((size, embed_size), dtype=torch.float32)
+        self.text_memory_bank = torch.zeros((size, embed_size), dtype=torch.float32)
+        self.index_pointer = 0
+        self.age = torch.zeros(size, dtype=torch.long)
 
-    def compute_loss(self, logit_scale:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        img_emb = torch.cat(self.acc_img_emb, 0)
-        text_emb = torch.cat(self.acc_text_emb, 0)
+    def _update(self, img_emb:torch.Tensor, text_emb:torch.Tensor) -> None:
+        end_idx = self.index_pointer + self.batch_size
+        self.image_memory_bank[self.index_pointer:end_idx] = img_emb
+        self.text_memory_bank[self.index_pointer:end_idx] = text_emb
+        self.age[self.index_pointer:end_idx] = 0
+        self.age += 1 # new samples always have an age of 1
+        if end_idx == self.size-1:
+            self.index_pointer = 0
+        else:
+            self.index_pointer = end_idx
 
-        self.acc_img_emb = []
-        self.acc_text_emb = []
+    def _get_decay(self) -> float:
+        if not self.schedule_decay_rate:
+            return self.decay_rate
+        return self.initial_decay_rate + (self.final_decay_rate - self.initial_decay_rate) * (self.current_step / self.total_steps)
 
-        logits_per_image = logit_scale.exp() * img_emb @ text_emb.t()
-        logits_per_text = logits_per_image.t()
+    def compute_loss(
+            self,
+            logit_scale:torch.Tensor,
+            img_emb:torch.Tensor,
+            text_emb:torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        assert img_emb.size(0) == text_emb.size(0) == self.batch_size
+
+        # current samples always have an age of 0
+        weights = torch.concat([torch.zeros(self.batch_size, device=self.age.device), self.age], dim=0)
+        weights = torch.pow(self.decay_rate, weights.float())
+        weights = weights / weights.sum()
+        
+        scale = logit_scale.exp()
+        logits_per_image = scale * img_emb @ torch.concat([text_emb, self.text_memory_bank], dim=0).t()
+        logits_per_text = scale * text_emb @ torch.concat([img_emb, self.image_memory_bank], dim=0).t()
 
         target = torch.arange(len(logits_per_image)).long().to(logits_per_image.device)
 
@@ -25,11 +60,9 @@ class ContrastiveLearningModule:
         text_itc_acc = (logits_per_text.argmax(dim=1) == target).float().mean()
 
         itc_loss = (
-            F.cross_entropy(logits_per_image.float(), target)
-            + F.cross_entropy(logits_per_text.float(), target)
+            F.cross_entropy(logits_per_image.float()*weights, target, reduction='none')
+            + F.cross_entropy(logits_per_text.float()*weights, target, reduction='none')
         ) / 2
+
+        self._update(img_emb, text_emb)
         return itc_loss, img_itc_acc, text_itc_acc
-    
-    def add_emb_pair(self, img_emb:torch.Tensor, text_emb:torch.Tensor) -> None:
-        self.acc_img_emb.append(img_emb)
-        self.acc_text_emb.append(text_emb)
