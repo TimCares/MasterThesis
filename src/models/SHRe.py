593eab3488f2bccca58bef8ee96623c93deb1dba
Tim Cares <peep-kestrel-0t@icloud.com>
2024-06-22 22:02:31 +0200

    Adjustments

--- src/models/SHRe.py
+++ src/models/SHRe.py
@@ -5,18 +5,15 @@ from functools import partial
 from typing import Dict, Any, List
 import numpy as np
 import os
-from utils import load_pretrained_d2v_model, get_d2v_image_embed, get_d2v_text_embed
-from omegaconf import OmegaConf
+from utils import load_pretrained_d2v_model
 import logging
 import pytorch_lightning as L
 from dataclasses import dataclass, field
 from data2vec_fairseq.data.modality import Modality
 from transformers.optimization import get_cosine_schedule_with_warmup, get_constant_schedule_with_warmup
-from modules import MOMEAltBlock
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
 import timm
-from timm.layers import PatchEmbed, Mlp
-from fairseq.data.dictionary import Dictionary
+from timm.layers import Mlp
 
 logger = logging.getLogger(__name__)
 
@@ -142,29 +139,24 @@ class SHRePreTrainingLightningModule(L.LightningModule):
         super().log(batch_size=self.cfg.data.dataloader.batch_size, sync_dist=True, *args, **kwargs)
 
 @dataclass
+class PretrainedStateDictsConfig():
+    audio:str = 'base_libri.pt'
+    image:str = 'base_imagenet.pt'
+    text:str = 'nlp_base.pt'
+
+@dataclass
 class SHReConfig():
     pretrained_path:str = '../models'
+    pretrained: PretrainedStateDictsConfig = field(default_factory=PretrainedStateDictsConfig)
 
     itc: bool = True
 
     embed_dim: int = 768
 
     depth: int = 6
-    num_heads: int = 12
     mlp_ratio: float = 4.0
-    encoder_dropout: float = 0.1
-    attention_dropout: float = 0.1
-    activation_dropout: float = 0.0
-    post_mlp_drop: float = 0.1
     norm_eps: float = 1e-6
     norm_affine: bool = True
-    layer_norm_first: bool = False
-    dropout_input: float = 0.0
-    start_drop_path_rate: float = 0
-    end_drop_path_rate: float = 0
-    layerdrop: float = 0.0
-
-    seed: int = 42
 
 class SHRe(nn.Module):
     def __init__(self,
@@ -174,22 +166,6 @@ class SHRe(nn.Module):
         self.cfg = cfg
         self.supported_modalities = [Modality.IMAGE, Modality.TEXT]
 
-        self.patch_embed = PatchEmbed(
-            img_size=224,
-            patch_size=16,
-            in_chans=3,
-            embed_dim=self.cfg.embed_dim,
-        )
-        self.img_pos_embed = nn.Parameter(torch.zeros(1, 1 + self.patch_embed.num_patches, self.cfg.embed_dim), requires_grad=False)
-        self.img_norm = nn.LayerNorm(self.cfg.embed_dim, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine)
-
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.cfg.embed_dim))
-
-        d = Dictionary.load('/workspace/dict.txt')
-        self.token_embed = nn.Embedding(len(d), self.cfg.embed_dim, d.pad())
-        self.text_pos_embed = nn.Embedding(512, self.cfg.embed_dim, d.pad())
-        self.text_norm = nn.LayerNorm(self.cfg.embed_dim, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine)
-
         self.shared = Mlp(
             in_features=self.cfg.embed_dim,
             hidden_features=int(self.cfg.embed_dim*self.cfg.mlp_ratio),
@@ -197,65 +173,12 @@ class SHRe(nn.Module):
             norm_layer=partial(nn.LayerNorm, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine),
         )
 
-        self.dropout_input = nn.Dropout(self.cfg.dropout_input)
-
-        dpr = np.linspace(self.cfg.start_drop_path_rate, self.cfg.end_drop_path_rate, self.cfg.depth)
-
-        blocks:List[MOMEAltBlock] = []
-        for i in range(self.cfg.depth):
-            blocks.append(self.make_block(drop_path=dpr[i], multimodal=False))
-
-        self.blocks:nn.ModuleList = nn.ModuleList(blocks)
-
-        self.layerdrop = self.cfg.layerdrop
-
         self.apply(init_bert_params)
 
-        self._init_from_pretrained()
-        
-    def make_block(self, drop_path, dim=None, heads=None, multimodal=False, with_fuzed=False, shared_attn=True):
-        make_layer_norm = partial(
-            nn.LayerNorm, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine
-        )
-
-        return MOMEAltBlock(
-            self.cfg.embed_dim if dim is None else dim,
-            self.cfg.num_heads if heads is None else heads,
-            self.cfg.mlp_ratio,
-            qkv_bias=True,
-            drop=self.cfg.encoder_dropout,
-            attn_drop=self.cfg.attention_dropout,
-            mlp_drop=self.cfg.activation_dropout,
-            post_mlp_drop=self.cfg.post_mlp_drop,
-            drop_path=drop_path,
-            norm_layer=make_layer_norm,
-            layer_norm_first=self.cfg.layer_norm_first,
-            multimodal=multimodal,
-            with_fuzed=with_fuzed,
-            shared_attn=shared_attn,
-        )
-
-    def _init_from_pretrained(self) -> None:
-        for modality in self.supported_modalities:
-            state_dict_name = self.cfg.pretrained[modality.name.lower()]
-            state_dict_path = os.path.join(self.cfg.pretrained_path, state_dict_name)
-            d2v_model = load_pretrained_d2v_model(state_dict_path=state_dict_path)
-
-            for i in range(self.cfg.depth):
-                self.blocks[i].init_from_pretrained(
-                    pretained_block=d2v_model.blocks[i],
-                    modality=modality,
-                    init_attention=True,
-                )
-
-        text_embed_sd = get_d2v_text_embed()
-        img_embed_sd = get_d2v_image_embed()
-        embed_sd = text_embed_sd | img_embed_sd
-        _, unexpected_keys = self.load_state_dict(embed_sd, strict=False)
-        assert len(unexpected_keys) == 0
-        for module in [self.patch_embed, self.img_pos_embed, self.cls_token, self.img_norm,
-                       self.token_embed, self.text_pos_embed, self.text_norm]:
-            self._freeze(module)
+        self.text_model = load_pretrained_d2v_model(state_dict_path=os.path.join(self.cfg.pretrained_path, self.cfg.pretrained.text))
+        self.text_model.blocks = self.text_model.blocks[:self.cfg.depth]
+        self.image_model = load_pretrained_d2v_model(state_dict_path=os.path.join(self.cfg.pretrained_path, self.cfg.pretrained.image))
+        self.image_model.blocks = self.image_model.blocks[:self.cfg.depth]
 
     def forward(
         self,
@@ -278,36 +201,19 @@ class SHRe(nn.Module):
         return out
     
     def encode_image(self, image):
-        x = self.patch_embed(image)
-        x = x + self.cls_token.expand(x.shape[0], -1, -1)
-        x = x + self.img_pos_embed
-        x = self.img_norm(x)
-
-        if self.dropout_input is not None:
-            x = self.dropout_input(x)
-        
-        for i in range(self.cfg.depth):
-            x, _ = self.blocks[i](
-                x,
-                modality=Modality.IMAGE,
-            )
+        x = self.image_model.extract_features(
+            source=image,
+            remove_extra_tokens=False,
+        )['x']
         
         return self.encode_shared(x)
 
     def encode_text(self, text, padding_mask):
-        x = self.token_embed(text)
-        x = x + self.text_pos_embed(text)
-        x = self.text_norm(x)
-
-        if self.dropout_input is not None:
-            x = self.dropout_input(x)
-        
-        for i in range(self.cfg.depth):
-            x, _ = self.blocks[i](
-                x,
-                modality=Modality.TEXT,
-                padding_mask=padding_mask,
-            )
+        x = self.text_model.extract_features(
+            source=text,
+            padding_mask=padding_mask,
+            remove_extra_tokens=False,
+        )['x']
         
         return self.encode_shared(x)
     
