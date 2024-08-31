c48e25f2dc9f0377d4771bc1ca3e5ac6ddcd13b6
Tim Cares <peep-kestrel-0t@icloud.com>
2024-08-31 13:20:20 +0200

    Deleted old files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,8 +2,6 @@ from .model_registry import MODEL_REGISTRY
 from .SHRe import SHRePreTrainingLightningModule, SHReConfig
 from .Sx3HRe import Sx3HRePreTrainingLightningModule, Sx3HReConfig
 from .image_vq import ImageVQLightningModule, ImageVQConfig
-from .ivq_l import ImageVQLLightningModule, ImageVQLConfig
-from .ivq_l_contrast import ImageVQLContrastLightningModule, ImageVQLContrastConfig
 from .image_cluster import ImageCluster
 from .text_kd import TextKDPreTrainingLightningModule, TextKDConfig
 from .image_kd import ImageKDPreTrainingLightningModule, ImageKDConfig
