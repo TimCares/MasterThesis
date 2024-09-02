13b2febd3d21b0736ab6aaac026ee0e7e36559a4
Tim Cares <peep-kestrel-0t@icloud.com>
2024-09-02 17:19:48 +0200

    adjusted glue sweep sh script

--- src/sweep_glue.sh
+++ src/sweep_glue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 for cfg in cola.yaml mnli_m.yaml mnli_mm.yaml mrpc.yaml qnli.yaml qqp.yaml rte.yaml sst.yaml stsb.yaml; do
-    python run_text_finetuning.py --config-name $cfg
+    python run_unimodal_train.py --config-path ../configs/fine_tuning --config-name $cfg
