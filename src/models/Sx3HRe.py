4f274d11c3da6e914e796507a33008916c7f91e8
Tim Cares <peep-kestrel-0t@icloud.com>
2024-09-05 18:07:59 +0200

    Restarted SHRe

--- src/models/Sx3HRe.py
+++ src/models/Sx3HRe.py
@@ -3,22 +3,21 @@ from torch import nn
 import torch.nn.functional as F
 from functools import partial
 from typing import Dict, Any
-import os
 import numpy as np
-from transformers import BertModel
+from omegaconf import OmegaConf
+import os
 import logging
 import pytorch_lightning as L
+import json
 from dataclasses import dataclass, field
 from data2vec_fairseq.data.modality import Modality
 from transformers.optimization import get_cosine_schedule_with_warmup
 from fairseq.modules.transformer_sentence_encoder import init_bert_params
-from omegaconf import OmegaConf
+from transformers import BertModel
 from . import MODEL_REGISTRY
-from modules import Block, ClipLoss, KDClipMomentumMemoryBankLoss, KDClipLoss
-from timm.models.vision_transformer import LayerScale
-from utils import freeze_module, load_beit2_teacher
+from modules import Block, ClipLoss
+from utils import freeze_module, load_beit2_teacher, load_pretrained_d2v_model
 from beit2.modeling_pretrain import VisionTransformerForMaskedImageModeling
-from utils import load_pretrained_d2v_model
 
 logger = logging.getLogger(__name__)
 
@@ -38,31 +37,15 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
             sd_path=beit_path,
             **beit2_kwargs,
         )
-        self.teacher.norm = nn.Identity()
         freeze_module(self.teacher)
-
-        self.logit_scale_target = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
 
         self.save_hyperparameters()
 
     def on_train_start(self):
         logger.info(f'World size: {self.trainer.world_size}')
-        logger.info(f'Local rank: {self.trainer.local_rank}')
         self.clip_loss = ClipLoss(
             cache_labels=True,
             rank=self.trainer.local_rank,
-            world_size=self.trainer.world_size
-        )
-        self.kd_loss_clip = KDClipLoss(
-            cache_labels=True,
-            rank=self.trainer.local_rank,
-            world_size=self.trainer.world_size
-        )
-
-        self.kd_loss_mb = KDClipMomentumMemoryBankLoss(
-            embed_size=self.cfg.model.embed_dim,
-            size=65536,
-            device=self.device,
             world_size=self.trainer.world_size
         )
 
@@ -79,15 +62,15 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
         if 'target' in batch:
             batch.pop('target') # unused, layer activations are the targets
 
-        image = batch['image']
+        image_teacher = batch['image_teacher']
 
         # no mask
-        bool_masked_pos = torch.zeros((image.shape[0], self.teacher.patch_embed.num_patches), 
-                                      dtype=torch.bool).to(image.device)
+        bool_masked_pos = torch.zeros((image_teacher.shape[0], self.teacher.patch_embed.num_patches), 
+                                      dtype=torch.bool).to(image_teacher.device)
 
         with torch.no_grad():
             target = self.teacher.forward_features(
-                x=image,
+                x=image_teacher,
                 bool_masked_pos=bool_masked_pos,
             )[:, 0]
         
@@ -95,54 +78,26 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
         input_text = output_dict['encoder_out_text']
         input_image = output_dict['encoder_out_image']
 
-        input_text = input_text / input_text.norm(dim=-1, keepdim=True)
-        input_image = input_image / input_image.norm(dim=-1, keepdim=True)
-        target = target / target.norm(dim=-1, keepdim=True)
-
-        if stage == 'train':
-            kd_out = self.kd_loss_mb(
-                input_image=input_image,
-                input_text=input_text,
-                target=target,
-                logit_scale=self.logit_scale_target.exp(),
-            )
-            self.kd_loss_mb._update(target=target)
-        else:
-            kd_out = self.kd_loss_clip(
-                input_image=input_image,
-                input_text=input_text,
-                target=target,
-                logit_scale=self.logit_scale_target.exp(),
-            )
-
-
-        self.log(f"{stage}/kd_text_loss", kd_out['text_loss'])
-        self.log(f"{stage}/kd_image_loss", kd_out['image_loss'])
-        kd_loss = kd_out['loss']
+        kd_loss1 = F.mse_loss(input=input_text, target=target)
+        self.log(f"{stage}/kd_text_loss", kd_loss1)
+        kd_loss2 = F.mse_loss(input=input_image, target=target)
+        self.log(f"{stage}/kd_image_loss", kd_loss2)
+        kd_loss = (kd_loss1 + kd_loss2) / 2
         self.log(f"{stage}/kd_loss", kd_loss)
 
-        self.log_kd_acc(kd_out['logits_per_image'], kd_out['logits_per_text'], kd_out['targets'], stage)
-
-        self.model.logit_scale_interm.data.clamp_(0, 4.6052) # as per FLAVA, also max value of VLMo
-        self.model.logit_scale_out.data.clamp_(0, 4.6052) # as per FLAVA, also max value of VLMo
+        self.model.logit_scales.data.clamp_(0, 4.6052) # as per FLAVA, also max value of VLMo
 
         if stage == 'train':
-            itc_out1 = self.clip_loss(
-                image_features=output_dict['x_interm_image'],
-                text_features=output_dict['x_interm_text'],
-                logit_scale=self.model.logit_scale_interm.exp(),
-            )
-
-            itc_out2 = self.clip_loss(
-                image_features=output_dict['x_image'],
-                text_features=output_dict['x_text'],
-                logit_scale=self.model.logit_scale_out.exp(),
-            )
-
-            self.log_itc_acc(itc_out1['logits_per_image'], itc_out1['logits_per_text'], itc_out1['targets'], stage, key_prefix="interm")
-            self.log_itc_acc(itc_out2['logits_per_image'], itc_out2['logits_per_text'], itc_out2['targets'], stage)
-                
-            itc_loss = (itc_out1['loss'] + itc_out2['loss']) / 2
+            itc_loss = 0
+            for i, key_prefix in enumerate(['x_interm', 'x']):
+                itc_out = self.clip_loss(
+                    image_features=output_dict[key_prefix + '_image'],
+                    text_features=output_dict[key_prefix + '_text'],
+                    logit_scale=self.model.logit_scales[i].exp(),
+                )
+                self.log_itc_acc(itc_out['logits_per_image'], itc_out['logits_per_text'], itc_out['targets'], stage, key_prefix=key_prefix)
+                itc_loss += itc_out['loss']
+            itc_loss /= 2
             self.log(f"{stage}/itc_loss", itc_loss)
         else:
             itc_loss = 0
@@ -162,14 +117,6 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
         self.log(f"{stage}/{key_prefix}itc_image_acc", img_itc_acc)
         self.log(f"{stage}/{key_prefix}itc_acc", (img_itc_acc + text_itc_acc) / 2, prog_bar=True)
 
-    def log_kd_acc(self, logits_per_image, logits_per_text, target, stage):
-        img_itc_acc = (logits_per_image.argmax(dim=1) == target).float().mean()
-        text_itc_acc = (logits_per_text.argmax(dim=1) == target).float().mean()
-        
-        self.log(f"{stage}/kd_text_acc", text_itc_acc)
-        self.log(f"{stage}/kd_image_acc", img_itc_acc)
-        self.log(f"{stage}/kd_acc", (img_itc_acc + text_itc_acc) / 2, prog_bar=True)
-
     def configure_optimizers(self):
         ws = torch.cuda.device_count()
         logger.info(f"[Optimizer]: World size is {ws}")
@@ -180,13 +127,9 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
             learning_rate = self.cfg.optimizer.base_lr * (self.cfg.data.dataloader.batch_size*ws) / 256
             logger.info(f"[Optimizer]: Base Learning rate is {self.cfg.optimizer.base_lr}")
         logger.info(f"[Optimizer]: Learning rate is {learning_rate}")
-        wd_params, non_wd_params = self._get_param_groups()
-        assert len(wd_params) + len(non_wd_params) == len(list(self.model.parameters()))
+        param_groups = self._get_param_groups(lr=learning_rate)
         optim_args = {
-            "params": [
-                {"params": wd_params, "weight_decay": self.cfg.optimizer.weight_decay},
-                {"params": non_wd_params, "weight_decay": 0}
-            ],
+            "params": param_groups,
             "lr": learning_rate,
             "betas": tuple(self.cfg.optimizer.betas),
             "eps": self.cfg.optimizer.eps,
@@ -205,14 +148,37 @@ class Sx3HRePreTrainingLightningModule(L.LightningModule):
         )
         return [optimizer], [{"scheduler": scheduler, "interval": "step", "name": 'cosine'}]
         
-    def _get_param_groups(self):
-        wd_params, non_wd_params = [], []
+    def _get_param_groups(self, lr):
+        parameter_group_names = {}
+        parameter_group_vars = {}
+
         for name, param in self.model.named_parameters():
-            if len(param.shape) == 1 or name.endswith(".bias") or "pos_embed" in name or "cls_token" in name or 'embeddings' in name:
-                non_wd_params.append(param)
+            if not param.requires_grad:
+                continue
+            if len(param.shape) == 1 or name.endswith(".bias") or "fixed_positional_encoder.positions" in name or "extra_tokens" in name or 'embeddings' in name:
+                group_name = "no_decay"
+                this_weight_decay = 0.
             else:
-                wd_params.append(param)
-        return wd_params, non_wd_params
+                group_name = "decay"
+                this_weight_decay = self.cfg.optimizer.weight_decay
+
+            if group_name not in parameter_group_names:
+                parameter_group_names[group_name] = {
+                    "weight_decay": this_weight_decay,
+                    "params": [],
+                    "lr": lr
+                }
+                parameter_group_vars[group_name] = {
+                    "weight_decay": this_weight_decay,
+                    "params": [],
+                    "lr": lr
+                }
+
+            parameter_group_vars[group_name]["params"].append(param)
+            parameter_group_names[group_name]["params"].append(name)
+
+        logger.info(f"Param groups = {json.dumps(parameter_group_names, indent=2)}")
+        return list(parameter_group_vars.values())
     
     def state_dict(self, *args, **kwargs):
         sd = super().state_dict(*args, **kwargs)
@@ -239,13 +205,7 @@ class Sx3HReConfig():
     beitv2: BEiTv2Config = field(default_factory=BEiTv2Config)
 
     embed_dim: int = 768
-
     depth: int = 6
-    num_heads: int = 12
-    mlp_ratio: float = 4.0
-    norm_eps: float = 1e-6
-    norm_affine: bool = True
-    layer_init_scale: float = 0.2
 
 class Sx3HRe(nn.Module):
     def __init__(self,
@@ -253,27 +213,20 @@ class Sx3HRe(nn.Module):
                  ):
         super(Sx3HRe, self).__init__()
         self.cfg = cfg
-        make_layer_norm = partial(nn.LayerNorm, eps=self.cfg.norm_eps, elementwise_affine=self.cfg.norm_affine)
-
-        self.token_type_embeddings = nn.Embedding(2, self.cfg.embed_dim)
-        self.tte_scale = LayerScale(self.cfg.embed_dim, init_values=1e-5)
-
-        self.proj_norm = make_layer_norm(self.cfg.embed_dim)
-        self.proj_head = nn.Linear(self.cfg.embed_dim, self.cfg.embed_dim)
+        make_layer_norm = partial(nn.LayerNorm, eps=1e-6)
 
         self.shared = Block(
             dim=self.cfg.embed_dim,
-            num_heads=self.cfg.num_heads,
-            mlp_ratio=self.cfg.mlp_ratio,
+            num_heads=12,
+            mlp_ratio=4.0,
             qkv_bias=True,
-            init_values=self.cfg.layer_init_scale,
+            init_values=0.2,
             norm_layer=make_layer_norm,
         )
 
         self.apply(init_bert_params)
 
-        self.logit_scale_interm = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
-        self.logit_scale_out = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.logit_scales = nn.Parameter(torch.ones([2]) * np.log(1 / 0.07))
 
         self.text_model = BertModel.from_pretrained("bert-base-uncased")
         self.text_model.encoder.layer = self.text_model.encoder.layer[:self.cfg.depth]
@@ -307,12 +260,6 @@ class Sx3HRe(nn.Module):
             source=image,
             remove_extra_tokens=False,
         )['x']
-
-        img_tte = self.token_type_embeddings(
-            torch.ones_like(x[:, :, 0], dtype=torch.long)
-        )
-        img_tte = self.tte_scale(img_tte)
-        x = x + img_tte
         
         return self.encode_shared(x)
 
@@ -321,14 +268,8 @@ class Sx3HRe(nn.Module):
             input_ids=text,
             attention_mask=1-padding_mask,
         ).last_hidden_state
-
-        text_tte = self.token_type_embeddings(
-            torch.zeros_like(padding_mask)
-        )
-        text_tte = self.tte_scale(text_tte)
-        x = x + text_tte
         
-        return self.encode_shared(x, mask=padding_mask)
+        return self.encode_shared(x, padding_mask)
     
     def encode_shared(self, x, mask=None):
         out_dict = dict()
@@ -336,7 +277,7 @@ class Sx3HRe(nn.Module):
         x_interm = x_interm[:, 0]
         x = x[:, 0]
 
-        out_dict["encoder_out"] = self.proj_head(self.proj_norm(x))
+        out_dict["encoder_out"] = x
         x = x / x.norm(dim=-1, keepdim=True)
         out_dict["x"] = x
         x_interm = x_interm / x_interm.norm(dim=-1, keepdim=True)
