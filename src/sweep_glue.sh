b85f8563ea3d9931b8a48824d931b5b2a3f4e3ed
Tim Cares <peep-kestrel-0t@icloud.com>
2024-09-24 19:04:05 +0200

    added wnli to glue sweep

--- src/sweep_glue.sh
+++ src/sweep_glue.sh
@@ -2,6 +2,6 @@
 
 model="/workspace/models/text_kd/model-42000-0.0292-train.ckpt"
 
-for cfg in cola mnli mrpc qnli qqp rte sst stsb; do
+for cfg in cola mnli mrpc qnli qqp rte sst stsb wnli; do
     python run_unimodal_train.py --config-path ../configs/fine_tuning --config-name $cfg model.model_path=$model
 done
